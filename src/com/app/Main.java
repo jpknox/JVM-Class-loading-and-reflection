f494f79f684fa127333514018bbd5da2695785ff
J P Knox <joaoknox@hotmail.com>
2017-12-02 10:22:16 +0000

    Jar of quoter class.

---
+++ src/com/app/Main.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import com.jpknox.Quoter;
+
+public class Main {
+
+    public static void main(String[] args) {
+		Quoter quoter = new Quoter();
+	    quoter.echo("Echhooooo");
+	    quoter.boeing();
+    }
+}
